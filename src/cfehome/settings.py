5a5f61a4c3969077091af13dd20648cd8bbc122b
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-04-10 11:50:11 +0545

    Made sure to use environment variables using .env and python decoupling

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ib38!1c9yfvddmkwmce*9j9l5lvgvl8xr)306629j@006#7&o0'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG",cast=bool)
+
 
 ALLOWED_HOSTS = [
     ".railway.app" #something like https://saas.prod.railway.app
