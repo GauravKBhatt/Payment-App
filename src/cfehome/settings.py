c429920010bde4fa0c06b8fa72ef4009912a5daa
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-04-10 12:23:08 +0545

    New Thing didn't work

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY",default=None)
+SECRET_KEY = config("DJANGO_SECRET_KEY",default='django-insecure-ib38!1c9yfvddmkwmce*9j9l5lvgvl8xr)306629j@006#7&o0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG",cast=bool,default=True)
