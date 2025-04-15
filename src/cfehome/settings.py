0309e738db67e98a5d2a3818a1625e2a41db94f1
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-04-15 11:53:20 +0545

    hrthaerth

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 import os
 from decouple import Config
-config=Config()
+config = Config(search_path='C:\Users\LEGION\Desktop\SAAS\src\cfehome\.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
